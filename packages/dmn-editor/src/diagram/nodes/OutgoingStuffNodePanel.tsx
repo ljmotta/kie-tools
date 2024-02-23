099165883ec76944615968e82b160dd91c660273
Luiz Motta <luizjoaomotta@gmail.com>
2024-02-23 15:29:12 -0300

    Rollback title on outgoing stuff node panel

--- packages/dmn-editor/src/diagram/nodes/OutgoingStuffNodePanel.tsx
+++ packages/dmn-editor/src/diagram/nodes/OutgoingStuffNodePanel.tsx
@@ -66,6 +66,55 @@ export function OutgoingStuffNodePanel(props: {
     [props.isVisible]
   );
 
+  const getEdgeActionTitle = React.useCallback((edgeType: string): string => {
+    switch (edgeType) {
+      case EDGE_TYPES.informationRequirement: {
+        return "Add Information Requirement edge";
+      }
+      case EDGE_TYPES.knowledgeRequirement: {
+        return "Add Knowledge Requirement edge";
+      }
+      case EDGE_TYPES.authorityRequirement: {
+        return "Add Authority Requirement edge";
+      }
+      case EDGE_TYPES.association: {
+        return "Add Association edge";
+      }
+      default: {
+        return "Add Unknon edge type";
+      }
+    }
+  }, []);
+
+  const getNodeActionTitle = React.useCallback((nodeType: string): string => {
+    switch (nodeType) {
+      case NODE_TYPES.inputData: {
+        return "Add Input Data node";
+      }
+      case NODE_TYPES.decision: {
+        return "Add Decision node";
+      }
+      case NODE_TYPES.bkm: {
+        return "Add Business Knowledge Model node";
+      }
+      case NODE_TYPES.decisionService: {
+        return "Add Decision Service node";
+      }
+      case NODE_TYPES.knowledgeSource: {
+        return "Add Knowledge Source node";
+      }
+      case NODE_TYPES.textAnnotation: {
+        return "Add Text Annotation node";
+      }
+      case NODE_TYPES.group: {
+        return "Add Group node";
+      }
+      default: {
+        return "Add Unkonw node type";
+      }
+    }
+  }, []);
+
   return (
     <>
       <Flex className={"kie-dmn-editor--outgoing-stuff-node-panel"} style={style}>
@@ -79,6 +128,7 @@ export function OutgoingStuffNodePanel(props: {
                 type={"source"}
                 style={handleStyle}
                 position={RF.Position.Top}
+                title={getEdgeActionTitle(edgeType)}
                 data-testid={`#${props.nodeId}-add-${edgeType}`}
               >
                 <svg
@@ -117,6 +167,7 @@ export function OutgoingStuffNodePanel(props: {
                 type={"source"}
                 style={handleStyle}
                 position={RF.Position.Top}
+                title={getNodeActionTitle(nodeType)}
                 data-testid={`#${props.nodeId}-add-${nodeType}`}
               >
                 <svg
